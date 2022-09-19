60743e1d2ecfd52ba8aee473d00cf7fc3b3c10fc
DJ Garcia <dj@employbridge.com>
2022-09-18 19:03:27 -0500

    Initial working console config app

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args).Build();
+
+// Ask the service provider for the configuration abstraction.
+IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+// Get values from the config given their key and their target type.
+// This next 6 lines of code will pull values from appsettings.json
+int keyOneValue = config.GetValue<int>("KeyOne");
+bool keyTwoValue = config.GetValue<bool>("KeyTwo");
+string keyThreeNestedValue = config.GetValue<string>("KeyThree:Message");
+
+// Write the values to the console.
+Console.WriteLine($"KeyOne = {keyOneValue}");
+Console.WriteLine($"KeyTwo = {keyTwoValue}");
+Console.WriteLine($"KeyThree:Message = {keyThreeNestedValue}");
+
+Console.WriteLine("Hello, World!");
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # lab-dotnet-console-config
 .NET Core 6 Console Configuration Management using IHost
+
+## Manual Project Setup
+
+```bash
+mkdir lab-dotnet-console-config
+cd lab-dotnet-console-config
+dotnet new console
+dotnet add package Microsoft.Extensions.Hosting
+code .
+```
+
+Copy/Paste Program.cs & appsettings.json code
